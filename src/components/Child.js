fd90ec4ee3397a9aefc22e08da40fa4dafb1dddd
GAURAV GOYAL <116831689+gauravgoyal467@users.noreply.github.com>
2023-07-13 11:06:24 +0000

    ok

--- src/components/Child.js
+++ src/components/Child.js
@@ -9,7 +9,7 @@ const Child = ({showModal,setShowModal}) => {
         {showModal && 
         <div>
             <h3>Modal Content</h3>
-            <h3>This is modal content</h3>
+            <p>This is modal content</p>
         </div>}
         
     </div>
