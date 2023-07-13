c42b667fcac51e37e7995c89329ba3eab9748ede
GAURAV GOYAL <116831689+gauravgoyal467@users.noreply.github.com>
2023-07-13 10:56:39 +0000

    final solution

--- src/components/Child.js
+++ src/components/Child.js
@@ -1,19 +1,16 @@
 import React from "react";
 
 const Child = ({showModal,setShowModal}) => {
-   const handleBtnClick=()=>{
-    setShowModal(!showModal);
-    }
-
+   
   return (
     <div className="child">
         <h2>Child Component</h2>
-        <button onclick={()=>handleBtnClick()} >Show Modal</button>
-        showModal && 
-        (<div>
+        <button onClick={()=>setShowModal(!showModal)} >Show Modal</button>
+        {showModal && 
+        <div>
             <h1>Modal Content</h1>
             <p>This is modal content</p>
-        </div>)
+        </div>}
         
     </div>
   )
